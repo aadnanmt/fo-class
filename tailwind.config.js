6cd05ab1aa6865f7ccfeb597af8b36d672785ab8
Adnan <aadnanmtcontact@gmail.com>
2026-02-03 18:37:57 +0700

    chore: configure tailwindcss standalone setting and theme

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content
+  content: [
+    "./app/templates/**/*.html",
+    "./app/modules/**/templates/**/*.html",
+    "./app/static/**/*.js"
+  ],
+  // theme
+  theme: {
+    extend: {
+      fontFamily: {
+        'main': ['"Orbitron"', 'sans-serif'],
+        'mono': ['"Share Tech Mono"', 'monospace'],
+      },
+      colors: {
+        'blue': '#00f3ff',
+        'pink': '#ff00ff',
+      },
+      animation: {
+        'marquee': 'marquee 10s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        }
+      }
+    },
+  },
+  plugins: [],
+}
