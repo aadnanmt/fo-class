e88d78e4740c0092954a721f5c3a4df8ef4bb0ed
aadnanmt <aadnanmtcontact@gmail.com>
2026-02-03 09:43:13 +0700

    chore: setup Supabase infrastructure layer

---
+++ app/infrastructure/supabase_client.py
@@ -0,0 +1,24 @@
+from supabase import create_client, Client
+import os
+
+# use connection pooling
+_supabase_client: Client = None
+
+def get_supabase_client() -> Client:
+    global _supabase_client
+
+    if _supabase_client is None:
+
+        # take credentials frm environment
+        url = os.environ.get("SUPABASE_URL")
+        key = os.environ.get("SUPABASE_KEY")
+        
+        
+        if not url or not key:
+            raise ValueError("Error: SUPABASE_URL or SUPABASE_KEY 'NOT FOUND' in file .env!")
+            
+        # create connection
+        _supabase_client = create_client(url, key)
+        print("Success, connected to Supabase")
+
+    return _supabase_client
