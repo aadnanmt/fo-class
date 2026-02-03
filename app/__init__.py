353e429eb656c9ca44d6d7492c18d86d3e2ad998
Adnan <aadnanmtcontact@gmail.com>
2026-02-03 18:44:20 +0700

    feat: setup flask application and blueprint registration

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,23 @@ from flask import Flask
 from .config import Config
 
 def create_app(config_class=Config):
+    
     # create object flask
     app = Flask(__name__)
     
     # insert configuration (from file config.py)
     app.config.from_object(config_class)
 
-    # Modul Home (pth: app/modules/home/__init__.py)
+    # Module Home (pth: app/modules/home/)
     from app.modules.home import home_bp
-    
-    # url_prefix='/'
-    app.register_blueprint(home_bp)
-    
+    app.register_blueprint(home_bp) # default url prefix ("/")
+
+    # Module messages (path: app/modules/messages/)
+    from app.modules.messages import messages_bp
+    app.register_blueprint(messages_bp, url_prefix='/messages')
+
+    # module gallery (path: app/modules/gallery/)
+    from app.modules.gallery import gallery_bp
+    app.register_blueprint(gallery_bp, url_prefix='/gallery')
+
     return app
