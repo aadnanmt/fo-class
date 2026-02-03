c669ec2115b2b16db1022d5a6b37670df4c900f6
aadnanmt <aadnanmtcontact@gmail.com>
2026-02-03 10:08:04 +0700

    chore: create module for pick credentials .env

---
+++ app/config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+# 1. load fil file .env to memory
+load_dotenv()
+
+class Config:
+    # pick SECRET_KEY from .env
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev_default_key')
+    
+    # pick setting Supabase
+    SUPABASE_URL = os.environ.get('SUPABASE_URL')
+    SUPABASE_KEY = os.environ.get('SUPABASE_KEY')
+
+    # Folder for upload photo (soon)
+    UPLOAD_FOLDER = os.path.join(os.getcwd(), 'app/static/uploads')
