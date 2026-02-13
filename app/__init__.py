e702539bc18f912a134b64896c5cb6c75295ec95
aadnanmt <aadnanmtcontact@gmail.com>
2026-02-13 14:05:51 +0700

    fix(human error): uncomment message and gallery bluprint

--- app/__init__.py
+++ app/__init__.py
@@ -16,11 +16,11 @@ def create_app(config_class=Config):
     app.register_blueprint(home_bp) # default url prefix ("/")
 
     # Module messages (path: app/modules/messages/)
-    from app.modules.messages import messages_bp
-    app.register_blueprint(messages_bp, url_prefix='/messages')
+    # from app.modules.messages import messages_bp
+    # app.register_blueprint(messages_bp, url_prefix='/messages')
 
     # module gallery (path: app/modules/gallery/)
-    from app.modules.gallery import gallery_bp
-    app.register_blueprint(gallery_bp, url_prefix='/gallery')
+    # from app.modules.gallery import gallery_bp
+    # app.register_blueprint(gallery_bp, url_prefix='/gallery')
 
     return app
