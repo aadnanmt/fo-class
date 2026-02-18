a1c42a72f4fe506ddcbbd2fa6a00d32c79d86f30
Adnan <aadnanmtcontact@gmail.com>
2026-02-19 05:23:57 +0700

    test: enable again modules for development

--- app/__init__.py
+++ app/__init__.py
@@ -15,12 +15,12 @@ def create_app(config_class=Config):
     from app.modules.home import home_bp
     app.register_blueprint(home_bp) # default url prefix ("/")
 
-    # Module messages (path: app/modules/messages/)
-    # from app.modules.messages import messages_bp
-    # app.register_blueprint(messages_bp, url_prefix='/messages')
+    Module messages (path: app/modules/messages/)
+    from app.modules.messages import messages_bp
+    app.register_blueprint(messages_bp, url_prefix='/messages')
 
-    # module gallery (path: app/modules/gallery/)
-    # from app.modules.gallery import gallery_bp
-    # app.register_blueprint(gallery_bp, url_prefix='/gallery')
+    module gallery (path: app/modules/gallery/)
+    from app.modules.gallery import gallery_bp
+    app.register_blueprint(gallery_bp, url_prefix='/gallery')
 
     return app
