b8467dd9d6d8566bb27cb015219b1abb07a61388
Adnan <aadnanmtcontact@gmail.com>
2026-02-19 05:59:29 +0700

    feat: service for message page

---
+++ app/modules/messages/services.py
@@ -0,0 +1,27 @@
+from . import repositories
+
+# list bad word
+BAD_WORD = ["bodoh", "jelek", "anjing", "kasar", "tolol",]
+
+def process_new_message(raw_content: str):
+    
+    # validation empety message
+    if not raw_content or not raw_content.strip():
+        raise ValueError("Pesan tidak boleh kosong!")
+
+    # max character
+    if len(raw_content) > 280:
+        raise ValueError("Pesan terlalu panjang (maks 280 karakter).")
+
+    # change BAD_WORD to **
+    clean_content = raw_content
+    for word in BAD_WORD:
+        # replace bad word with ****
+        clean_content = clean_content.replace(word, "*" * len(word))
+
+    # save message to db supabase
+    return repositories.add_message(clean_content)
+
+def get_messages_list():
+    # get all medsages from db supabase
+    return repositories.get_all_messages()
